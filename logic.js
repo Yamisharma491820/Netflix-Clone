46f31934a8d5f22c7cae9b595d85de39813ff153
Yamini Sharma <ys770462@gmail.com>
2022-09-30 11:28:24 +0530

    Initial commit

---
+++ logic.js
@@ -0,0 +1,91 @@
+// getElementsByClassName returns array of elements
+
+// function func(){
+//   var elms = document.getElementsByClassName("secli");
+
+//   Array.from(elms).forEach((x) => {
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   })
+// }
+
+//I have given different logics to different paragraphs
+
+function func(){
+  var elms = document.getElementsByClassName("secli1");
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+}else {
+      x.style.display = "none";
+    }
+  })
+
+  
+};
+
+function func2(){
+  var elms = document.getElementsByClassName("secli2");
+
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+};
+
+function func3(){
+  var elms = document.getElementsByClassName("secli3");
+
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+};
+
+function func4(){
+  var elms = document.getElementsByClassName("secli4");
+
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+}
+function func5(){
+  var elms = document.getElementsByClassName("secli5");
+
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+};
+function func6(){
+  var elms = document.getElementsByClassName("secli6");
+
+  Array.from(elms).forEach((x) => {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  })
+}
+
+
+
+
+
